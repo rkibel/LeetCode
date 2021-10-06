4e4c01ee1ec8122ce9a393460fbb0f6403688b56
Ron Kibel <ron.kibel1@gmail.com>
2021-10-05 22:30:17 -0700

    Create Problem442.java

---
+++ Problem442.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Problem442{
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0)
+                output.add(index + 1);
+            nums[index] = -nums[index];
+        }
+        return output;
+    }
+}
