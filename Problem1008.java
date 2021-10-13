0eee8eb4cc194aff4ac2bd5bb8bbc18a74bf73e4
Ron Kibel <ron.kibel1@gmail.com>
2021-10-12 19:43:01 -0700

    Create Problem1008.java

---
+++ Problem1008.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import javax.swing.tree.*;
+public class Problem1008 {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int val;
+        val = 5;
+        
+        
+        
+        
+        
+        TreeNode a = new TreeNode(val);
+        return a;
+    }
+}
