b6b5b39bc0030674cd47e109be87deb14b582cc9
Ron Kibel <ron.kibel1@gmail.com>
2021-10-13 11:21:09 -0700

    Update Problem1008.java

--- Problem1008.java
+++ Problem1008.java
@@ -17,14 +17,19 @@
 import javax.swing.tree.*;
 public class Problem1008 {
     public TreeNode bstFromPreorder(int[] preorder) {
-        int val;
-        val = 5;
-        
-        
-        
-        
-        
-        TreeNode a = new TreeNode(val);
-        return a;
+        TreeNode root = recursiveNode(preorder, 0, preorder.length-1);
+        return root;
+    }
+    public TreeNode recursiveNode (int[] preorder, int start, int end){
+        if (start > end)
+            return null;
+        TreeNode x = new TreeNode (preorder[start]);
+        int index = start + 1;
+        int val = preorder[start];
+        while (index <= end && preorder[index] < val)
+            index++;
+        x.left = recursiveNode (preorder, start+1, index-1);
+        x.right = recursiveNode (preorder, index, end);
+        return x;
     }
 }
