d808e359a638f3fd33546ba299397462812b5f8a
Ron Kibel <ron.kibel1@gmail.com>
2021-10-14 14:46:27 -0700

    Update Problem279.java

--- Problem279.java
+++ Problem279.java
@@ -1,18 +1,23 @@
 public class Problem279 {
-        public int numSquares(int n) {
-            return numSq (n, 0);        
-        }
-        int numSq (int n, int counter){
-            if (n == 0)
-                return counter;
-            if (n == 1)
-                return counter+1;
+    int m = Integer.MAX_VALUE;
+    public int numSquares(int n) {
+        numSq (n, 0);
+        return m;
+    }
+    void numSq (int n, int counter){
+        if (counter < m){
+            if (n <= 1)
+                m = Math.min(m, counter + n);
             else{
-                for (int i = 0; Math.pow(i, 2) <= counter; i++){
+                for (int i = SQRT(n); i >= 1; i--){
+                    //System.out.println(n);
                     int p = i*i;
                     numSq(n-p, counter+1);
                 }
             }
-            return 0;
         }
+    }
+    int SQRT (int num){
+        return (int)(Math.sqrt(num));
+    }
 }
