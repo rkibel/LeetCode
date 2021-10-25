471b131e721e61f157b68a157c872584140d674a
Ron Kibel <ron.kibel1@gmail.com>
2021-10-25 10:43:29 -0700

    Update Problem222.cpp

--- Problem222.cpp
+++ Problem222.cpp
@@ -9,24 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+#include <cmath>;
+class Problem222 {
 public:
+    struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
     int countNodes(TreeNode* root) {
         if (!root)
             return 0;
-        queue <TreeNode*> q;
-        q.push(root);
-        int count = 0;
-        while (!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            if (temp-> left && temp-> right)
-                count++;
-            if (temp->left != NULL)
-                q.push(temp->left);
-            if (temp->right != NULL)
-                q.push(temp->right);
+        TreeNode* tl = root, *tr = root;
+        int height_l = 0, height_r = 0;
+        while (tl){
+            tl = tl->left;
+            height_l++;
         }
-        return count;
+        while (tr){
+            tr = tr->right;
+            height_r++;
+        }
+        if (height_l == height_r)
+            return pow(2, height_r) - 1;
+        return 1+countNodes(root->left) + countNodes(root->right);
     }
 };
