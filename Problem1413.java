28cc0e51c675ba9683df38be38b56e129f28fa01
Ron Kibel <ron.kibel1@gmail.com>
2021-11-11 12:03:38 -0800

    Update Problem1413.java

--- Problem1413.java
+++ Problem1413.java
@@ -2,12 +2,10 @@ class Problem1413 {
     public int minStartValue(int[] nums) {
         int minSum = nums[0];
         int sum = nums[0];
-        int index = 0;
         for (int i = 1; i < nums.length; i++){
             sum += nums[i];
             if (sum <= minSum){
                 minSum = sum;
-                index = i;
             }
         }
         return (1 - minSum <= 0) ? 1 : 1-minSum;
