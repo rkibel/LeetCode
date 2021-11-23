c2db316fd9a02fe69f383ed254877c7ef43282db
Ron Kibel <ron.kibel1@gmail.com>
2021-11-22 22:09:42 -0800

    Update Problem450.cpp

--- Problem450.cpp
+++ Problem450.cpp
@@ -10,37 +10,28 @@ struct TreeNode {
 class Problem450 {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        TreeNode* found = findTree(root, key);
-        if (!found)
-            return nullptr;
-        TreeNode* replacer = traverseTree(found);
-        found->val = replacer->val;
-        return root;
+        TreeNode** result = findTree(root, key);
+        //case 1: there is no node with key
+        if (!result)
+            return root;
+        //case 2: the node with key has no children
+
+        //case 3: the node with key has only right children
+
+        //case 4: the node with key has only left children
+
+        //case 5: the node with key has both children
+        
     }
-    TreeNode* traverseTree(TreeNode*& curr){
-        TreeNode* output = nullptr;
-        if (curr->left){
-            TreeNode* result = traverseTree(curr->left);
-            (result != nullptr) ? output = result : output = nullptr;
+    TreeNode** findTree(TreeNode* tree, int key){
+        if (tree){
+            if (tree->val == key)
+                return &tree;
+            else if (tree->left && findTree(tree->left, key) != nullptr)
+                return findTree(tree->left, key);
+            else if (tree->right && findTree(tree->right, key) != nullptr)
+                return findTree(tree->right, key);
         }
-        else if (curr->right){
-            TreeNode* result = traverseTree(curr->right);
-            (result != nullptr) ? output = result : output = nullptr;
-        }
-        else{ 
-            output = curr;
-            curr = nullptr;
-        }
-        return output;
-    }
-    TreeNode* findTree(TreeNode* tree, int key){
-        TreeNode* output = nullptr;
-        if (tree->val == key)
-            output = tree;
-        else if (tree->left)
-            output = findTree(tree->left, key) != nullptr ? findTree(tree->left, key) : nullptr;
-        else if (tree->right)
-            output = findTree(tree->right, key) != nullptr ? findTree(tree->right, key) : nullptr;
-        return output;
+        return nullptr;
     }
 };
