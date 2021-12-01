3c5a77a9aeaa0417656e781424fb586cfcb6bd71
Ron Kibel <ron.kibel1@gmail.com>
2021-11-30 17:13:23 -0800

    Create Problem198.cpp

---
+++ Problem198.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Problem198 {
+    private:
+        int monay = 0;
+    public:
+        int rob(vector<int>& nums) {
+            findMax(nums, 0, 0);
+            return monay;
+        }
+        //brute force solution
+        void findMax(vector<int> nums, int index, int currSum){
+            if (nums.size() == 1){
+                monay = nums[0];
+                return;
+            }
+            if (nums.size() == 2){
+                monay = max(nums[0], nums[1]);
+                return;
+            }
+            if (index > nums.size()-1){
+                monay = max(monay, currSum);
+                return;
+            }
+            int val = nums.at(index);
+            if (index == nums.size()-2 || index == nums.size()-1){
+                monay = max(monay, currSum + val);
+            }
+            else{
+                findMax(nums, index+1, currSum);
+                findMax(nums, index+2, currSum + val);
+                findMax(nums, index+3, currSum + val);
+            }
+        }
+};
