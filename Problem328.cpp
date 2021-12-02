b9d6dbe4e4d0c4d0911a3acf1f158169e6cb7b38
Ron Kibel <ron.kibel1@gmail.com>
2021-12-01 22:02:17 -0800

    Create Problem328.cpp

---
+++ Problem328.cpp
@@ -0,0 +1,24 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Problem328 {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* odd = head, *even = head->next, *temp = even;
+        
+        while(even && even->next){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = temp;
+        return head;
+    }
+};
