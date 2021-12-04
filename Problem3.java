19164c33e5a91ba2fadaa86f14c17cc7b072637d
Ron Kibel <ron.kibel1@gmail.com>
2021-12-03 22:14:01 -0800

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -1,15 +1,35 @@
-public class Problem3 {
+class Problem3 {
     public int lengthOfLongestSubstring(String s) {
-        String maxString = "";
-        String currString = "";
+        //O(n^2) runtime solution
+        /*int m = 0;    
         for (int i = 0; i < s.length(); i++){
-            if (currString.indexOf(s.charAt(i)) == -1) 
-                currString += Character.toString(s.charAt(i));
-            else 
-                currString = Character.toString(s.charAt(i));
-            if (currString.length() > maxString.length()) 
-                maxString = currString;
+            String currString = Character.toString(s.charAt(i));
+            m = Math.max(m, currString.length());
+            for (int j = i+1; j < s.length(); j++){
+                if (currString.indexOf(s.charAt(j)) == -1){
+                    currString += s.charAt(j);
+                    m = Math.max(m, currString.length());
+                }
+                else j = s.length();
+            }
+            m = Math.max(m, currString.length());
         }
-    return maxString.length();
+        
+        return m;
+        */
+        //O(n) runtime solution
+        int m = 0;
+        String curr = "";
+        if (s.length() == 0) return m;
+        curr += s.charAt(0);
+        m = Math.max(m, curr.length());
+        for (int i = 1; i < s.length(); i++){
+            if (curr.indexOf(s.charAt(i)) != -1)
+                curr = curr.substring(curr.indexOf(s.charAt(i)) + 1) + Character.toString(s.charAt(i));
+            else
+                curr += s.charAt(i);
+            m = Math.max(m, curr.length());
+        }
+        return m;
     }
 }
