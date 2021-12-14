8ed60454decb9367cdf9c1cf1fac6a6824bca62a
Ron Kibel <ron.kibel1@gmail.com>
2021-12-13 18:17:23 -0800

    Create Problem938.cpp

---
+++ Problem938.cpp
@@ -0,0 +1,19 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        int tot = (root->val >= low && root->val <= high) ? root->val : 0;
+        if (root->left) tot += rangeSumBST(root->left, low, high);
+        if (root->right) tot += rangeSumBST(root->right, low, high);
+        return tot;
+    }
+};
