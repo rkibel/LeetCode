6f3a7c652fed97bc3dc9fd129d6c3555189beab3
Ron Kibel <ron.kibel1@gmail.com>
2021-12-16 12:30:35 -0800

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,5 +1,18 @@
+import java.util.*;
 class Problem1 {
     public int[] twoSum(int[] nums, int target) {
+        
+        // O(n) runtime solution
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(target-nums[i])) 
+                return new int[] {map.get(target-nums[i]), i};
+            map.put(nums[i], i);
+        }
+        return new int[] {-1, -1};
+
+        /**
+        *** O(n^2) runtime solution
         for (int i = 0; i < nums.length-1; i++){
             for (int j = i+1; j < nums.length; j++){
                 if (nums[i] + nums[j] == target){
@@ -7,6 +20,7 @@ class Problem1 {
                 }
             }
         }
-        return new int[0];
+        return new int[] {-1, -1};
+        **/
     }
 }
