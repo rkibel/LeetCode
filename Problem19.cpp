0bb92ccb3d9953e94780bb1adac9e1283ee2f912
Ron Kibel <ron.kibel1@gmail.com>
2021-12-22 09:19:23 -0800

    Create Problem19.cpp

---
+++ Problem19.cpp
@@ -0,0 +1,23 @@
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Problem19 {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int len = 0;
+        for (ListNode* i = head; i; i = i->next) len++;
+        len = len-n;
+        if (len == 0) return head->next;
+        ListNode* temp = head;
+        for (int i = 0; i < len-1; i++) temp = temp->next;
+        if (temp->next && temp->next->next) temp->next = temp->next->next;
+        else if (temp->next) temp->next = nullptr;
+        return head;
+    }
+};
