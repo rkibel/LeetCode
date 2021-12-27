e28d61a026151aa5cc1ebb0291792a7b7f80c0e3
Ron Kibel <ron.kibel1@gmail.com>
2021-12-27 12:02:38 -0800

    Update Problem100.cpp

--- Problem100.cpp
+++ Problem100.cpp
@@ -1,46 +1,17 @@
-/**
- * Definition for a binary tree node.
 struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-#include <algorithm>
-
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
 class Problem100 {
-    struct TreeNode {
-      int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    };
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        queue <TreeNode*> a;
-        queue <TreeNode*> b;
-        a.push(p);
-        b.push(q);
-        bool same = true;
-        if (a.size() != b.size())
-            same = false;
-        else {
-            while (!a.empty()) {
-                TreeNode* x1 = a.front();
-                a.pop();
-                TreeNode* x2 = b.front();
-                b.pop();
-                if (x1->val != x2->val){
-                    same = false;
-                    break;
-                }
-            }   
-        }
-        return same;
+        if (!p && !q) return true;
+        if ((p && !q) || (q  && !p) || (p->val != q->val)) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
