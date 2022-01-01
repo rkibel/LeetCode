b5f864dcba3039b05529017152f7c7117aa30d46
Ron Kibel <ron.kibel1@gmail.com>
2021-12-31 17:17:02 -0800

    Create Problem312.cpp

---
+++ Problem312.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int>points(n+2);
+        points[0] = 1, points[n+1] = 1;
+        for (int i = 0; i < n; i++) points[i+1] = nums[i];
+        
+        vector<vector<int>> dp(n+2, vector<int>(n+2));
+        for (int i = n; i >= 0; i--)
+            for (int j = i+1; j < n+2; j++)
+                for (int k = i+1; k < j; k++)
+                    dp[i][j] = max(dp[i][j], dp[i][k]+dp[k][j]+points[i]*points[k]*points[j]);
+                
+        return dp[0][n+1];
+    }
+};
