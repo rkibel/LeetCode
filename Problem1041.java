1fc1b625ad2621b03248a148a426503e2839cf1b
Ron Kibel <ron.kibel1@gmail.com>
2022-01-08 17:00:45 -0800

    Update Problem1041.java

--- Problem1041.java
+++ Problem1041.java
@@ -1,29 +1,22 @@
 class Problem1041 {
-    public boolean isRobotBounded(String ins) {
-        double dist = 0;
-        Pair<int[], Integer> pair = new Pair<>(new int[]{0,0}, 90);
-        for (int i = 0; i < 4; i++){
-            pair = getCoord(ins, pair.getKey(), pair.getValue());
-            dist = Math.max(dist, getDist(pair.getKey()));
-        }
-        return getDist(getCoord(ins, pair.getKey(), pair.getValue()).getKey()) <= dist;
+    int[] coord = {0,0};
+    int dir = 90, dist = 0;
+    public boolean isRobotBounded(String instructions) {
+        for (int i = 0; i < 4; i++) dist = Math.max(dist, loop(instructions));
+        return loop(instructions) <= dist;
     }
-    public Pair<int[], Integer> getCoord(String ins, int[] coord, int dir){
+    public int loop(String ins){
         for (int i = 0; i < ins.length(); i++){
-            if (ins.charAt(i) == 'L') dir = (dir + 90)%360;
+            if (ins.charAt(i) == 'L') dir = (dir+90)%360;
             else if (ins.charAt(i) == 'R') dir = (dir+270)%360;
-            else{
-                switch(dir){
-                    case 0: coord[0]++; break;
-                    case 90: coord[1]++; break;
-                    case 180: coord[0]--; break;
-                    case 270: coord[1]--; break;
-                }
+            else switch(dir){
+                case 0: coord[0]++; break;
+                case 90: coord[1]++; break;
+                case 180: coord[0]--; break;
+                case 270: coord[1]--; break;
+                default: throw new ArithmeticException("direction not parseable");
             }
         }
-        return new Pair<>(coord, dir);
-    }
-    public double getDist(int[]coord){
-        return Math.sqrt(coord[0]*coord[0] + coord[1]*coord[1]);
+        return coord[0]*coord[0] + coord[1]*coord[1];
     }
 }
