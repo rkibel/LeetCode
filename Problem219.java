37eed5049b122f4195a9a55a058f861511437f7e
Ron Kibel <ron.kibel1@gmail.com>
2022-01-12 00:18:35 -0800

    Create Problem219.java

---
+++ Problem219.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Problem219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i]) && (i - map.get(nums[i]) <= k)) return true;
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
