798fa53e06d903f2945b120047740697bb2b6614
Ron Kibel <ron.kibel1@gmail.com>
2022-01-11 19:10:39 -0800

    Create Problem701.cpp

---
+++ Problem701.cpp
@@ -0,0 +1,18 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) return new TreeNode(val);
+        (val < root->val) ? root->left = insertIntoBST(root->left, val) : 
+        root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
