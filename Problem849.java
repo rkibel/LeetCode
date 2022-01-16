36576a45feff6894ab25278167c640c48edadf3b
Ron Kibel <ron.kibel1@gmail.com>
2022-01-16 13:47:21 -0800

    Update Problem849.java

--- Problem849.java
+++ Problem849.java
@@ -1,14 +1,16 @@
 class Problem849 {
     public int maxDistToClosest(int[] seats) {
-        int curr = 0, maxDist = 0;
-        for (int i = 0; i < seats.length; i++){
+        int curr = 0;
+        while(seats[curr] == 0) curr++;
+        int maxDist = curr;
+        for (int i = curr; i < seats.length; i++){
             if (seats[i] == 1){
-                maxDist = Math.max(maxDist, (curr-1)/2);
+                maxDist = Math.max(maxDist, (curr+1)/2);
                 curr = 0;
             }
             else curr++;
         }
-        maxDist = Math.max(maxDist, curr-1);
-        return maxDist+1;
+        maxDist = Math.max(maxDist, curr);
+        return maxDist;
     }
 }
