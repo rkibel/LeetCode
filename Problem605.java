4a101e79d0a4cb6fecdd4af2eb09519cf33fb099
Ron Kibel <ron.kibel1@gmail.com>
2022-01-17 16:40:50 -0800

    Create Problem605.java

---
+++ Problem605.java
@@ -0,0 +1,13 @@
+class Problem605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++){
+            if (flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)){
+                flowerbed[i] = 1;
+                i++;
+                n--;
+            }
+            if (n <= 0) return true;
+        }
+        return false;
+    }
+}
