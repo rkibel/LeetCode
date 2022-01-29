f64733d6737708bf27d56f3597f098ba7276a006
Ron Kibel <ron.kibel1@gmail.com>
2022-01-28 19:31:15 -0800

    Update Problem84.java

--- Problem84.java
+++ Problem84.java
@@ -1,30 +1,5 @@
 import java.util.*;
-/*class Problem84 {
-    public int largestRectangleArea(int[] heights) {
-        int soln = 0;
-        for (int i = 0; i < heights.length; i++){
-            soln = Math.max(soln, maxArea(heights, i+1));
-        }
-        return soln;
-    }
-    public int mArray(int[]arr, int start, int end){
-        Integer m = null;
-        for (int i = start; i <= end; i++){
-            if (m == null) m = arr[i];
-            m = Math.min(arr[i], m);
-        }
-        return m;
-    }
-    public int maxArea(int[] arr, int nextTo){
-        int area = 0;
-        for (int i = 0; i < arr.length+1-nextTo; i++){
-            int minHeight = mArray(arr, i, i+nextTo-1);
-            area = Math.max(area, minHeight*nextTo);
-        }
-        return area;
-    }
-}
-*/
+
 class Problem84 {
     public int largestRectangleArea(int[] heights) {
         int len = heights.length;
