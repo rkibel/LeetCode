b85117a22480fa1d6db8c1033ed0179e38e6c9bf
Ron Kibel <ron.kibel1@gmail.com>
2023-08-21 13:01:51 -0700

    Update 459.py

--- 459.py
+++ 459.py
@@ -3,3 +3,6 @@ class Solution(object):
         for i in range(1, len(s)//2 + 1):
             if len(s) % i == 0 and s[0:i] * (len(s) // i) == s: return True
         return False
+
+        # or this:
+        # return any(len(s) % i == 0 and s[:i] * (len(s) // i) == s for i in range(1, len(s)//2 + 1))
