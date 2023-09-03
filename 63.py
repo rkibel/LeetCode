9f8ab6fe58b3b02c482ac977754b6ef7a0ce5278
Ron Kibel <ron.kibel1@gmail.com>
2023-09-03 17:09:58 +0200

    Update 63.py

--- 63.py
+++ 63.py
@@ -1,11 +1,11 @@
 class Solution(object):
-    def uniquePathsWithObstacles(self, obstacleGrid):
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[-1 if x == 0 else 0 for x in row] for row in obstacleGrid]
-        if dp[0][0] != 0: dp[0][0] = 1
-        def getPaths(i, j):
-            if i < 0 or j < 0: return 0
-            if dp[i][j] != -1: return dp[i][j]
-            dp[i][j] = getPaths(i-1, j) + getPaths(i, j-1)
-            return dp[i][j]
-        return getPaths(m-1, n-1)
+    def uniquePathsWithObstacles(self, obs):
+        if obs[0][0]: return 0
+        dp = [[1-x for x in row] for row in obs]
+        for i in range(len(obs)):
+            for j in range(len(obs[0])):
+                if (i == 0 and j == 0) or not dp[i][j]: continue
+                if i == 0: dp[i][j] = dp[i][j-1]
+                elif j == 0: dp[i][j] = dp[i-1][j]
+                else: dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
