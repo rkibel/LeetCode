f687bb806f942b610778eca564ed5be4badd4462
Ron Kibel <ron.kibel1@gmail.com>
2023-09-12 18:41:15 +0200

    Update 1359.py

--- 1359.py
+++ 1359.py
@@ -1,3 +1,4 @@
+from functools import reduce
 class Solution(object):
     def countOrders(self, n):
-        return reduce(lambda x, i: (x * ((2 * i - 1) % (10**9 + 7) * i % (10**9 + 7))) % (10**9 + 7), range(2, n+1), 1)
+        return reduce(lambda x, i: x * (2 * i - 1) * i % (10**9 + 7), range(2, n+1), 1)
